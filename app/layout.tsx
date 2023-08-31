b6515a5dfe44fcb389e5e441877c6849dc56cb47
Sam Selikoff <sam.selikoff@gmail.com>
2023-08-31 12:02:59 -0400

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full bg-gray-800 text-white antialiased">
+      <head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </head>
       <body className={`flex h-full items-center justify-center`}>
         {children}
       </body>
